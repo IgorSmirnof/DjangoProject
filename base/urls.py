6f241c4acf7bcf334df1fa9284b0588662892365
IgorSmirnof <igor.smirna@gmail.com>
2024-10-13 18:19:21 +0300

    first commit

---
+++ base/urls.py
@@ -0,0 +1,34 @@
+"""base URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from api.models import CourseResource, CategoryResource
+from tastypie.api import Api
+
+api = Api(api_name='v1')
+
+course_res = CourseResource()
+category_res = CategoryResource()
+api.register(CourseResource())
+api.register(CategoryResource())
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
+    # path('api/', include(course_res.urls)),
+    # path('api/', include(category_res.urls)),
+    path('api/', include(api.urls)),
+    ]
---
+++ shop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Course
+
+# Create your views here.
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'shop/courses.html', {'courses': courses})
+    # return HttpResponse("Hello user!")
+
+def single_course(request, course_id):
+    # course = Course.objects.get(pk=course_id)
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {'course': course})
+
